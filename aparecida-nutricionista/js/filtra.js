e0fc132a45176b2768c79d26631f0915612c8b9f
Magnólia <magnoliamedeiros@gmail.com>
2021-09-09 18:41:38 -0300

    Modificado o arquivo filtra.js adicionando expressão regular para melhorar a busca

--- aparecida-nutricionista/js/filtra.js
+++ aparecida-nutricionista/js/filtra.js
@@ -4,5 +4,27 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function(){
     console.log("Digitaram no campo!");
     var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            var expressao = new RegExp(this.value, "i");
     
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (let i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+            
+        }
+    }
 });
