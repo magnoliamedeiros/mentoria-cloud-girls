9cfef24b5b1b0e51724839d3a0e27eb7e1e45d94
Magnólia <magnoliamedeiros@gmail.com>
2021-09-09 18:07:48 -0300

    Criado a pasta javascript para reorganização dos arquivos

---
+++ javascript/verificando-tamanho-postagem/contador.js
@@ -0,0 +1,11 @@
+var campoPostagem = document.querySelector("#corpo-postagem");
+campoPostagem.addEventListener("input", atualizaCaracteres);
+
+function atualizaCaracteres() {
+    var postagem = document.querySelector("#corpo-postagem").value;
+
+    var caracteres = postagem.length;
+    var contador = document.querySelector("#numero-caracteres");
+
+    contador.innerHTML = caracteres;
+}
