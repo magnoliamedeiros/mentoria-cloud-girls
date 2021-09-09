384d9a1f39ff301974537af6c4af3db26a7b0343
Magnólia <magnoliamedeiros@gmail.com>
2021-09-09 18:12:57 -0300

    Criado a função de filtrar pelo nome do paciente

---
+++ aparecida-nutricionista/js/filtra.js
@@ -0,0 +1,8 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+// Monitora o evento de digitação 'input'
+campoFiltro.addEventListener("input", function(){
+    console.log("Digitaram no campo!");
+    var pacientes = document.querySelectorAll(".paciente");
+    
+});
