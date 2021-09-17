25d0e095b38d8ff9551c5d4e6c3d5cb67fded5bb
Magnólia <magnoliamedeiros@gmail.com>
2021-09-17 09:50:52 -0300

    Adicionando a logica para o campo de digitacao

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,4 +1,20 @@
 var frase = $(".frase").text();
 var quantidadePalavras = frase.split(" ").length;
-var tamanhoDaFrase = $("li");
-console.log(tamanhoDaFrase);
+var tamanhoDaFrase = $("#tamanho-frase");
+tamanhoDaFrase.text(quantidadePalavras);
+
+var campo = $(".campo-de-digitacao")
+
+// Quando a um click no campo 'click
+// Quando a um input no campo
+campo.on("input", function(){
+    // pega o valor (o que foi digitado)
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-de-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-de-caracteres").text(qtdCaracteres);
+
+});
