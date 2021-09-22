a1e1f8e60a6f100cc780042f581f64204dcddd22
Magnólia <magnoliamedeiros@gmail.com>
2021-09-22 07:45:31 -0300

    Adicionando id para tempo de digitacao e botao para reiniciar jogo

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,20 +1,67 @@
-var frase = $(".frase").text();
-var quantidadePalavras = frase.split(" ").length;
-var tamanhoDaFrase = $("#tamanho-frase");
-tamanhoDaFrase.text(quantidadePalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-de-digitacao")
 
-// Quando a um click no campo 'click
-// Quando a um input no campo
-campo.on("input", function(){
-    // pega o valor (o que foi digitado)
-    var conteudo = campo.val();
+// Aguarda o html da paǵina
+$(document).ready(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
 
-    var qtdPalavras = conteudo.split(/\S+/).length -1;
-    $("#contador-de-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-de-caracteres").text(qtdCaracteres);
-
+    // Com JavaScript
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var quantidadePalavras = frase.split(" ").length;
+    var tamanhoDaFrase = $("#tamanho-frase");
+    tamanhoDaFrase.text(quantidadePalavras);
+}
+
+function inicializaContadores() {
+
+    // Quando a um click no campo 'click
+    // Quando a um input no campo
+    campo.on("input", function(){
+        // pega o valor (o que foi digitado)
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length -1;
+        $("#contador-de-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-de-caracteres").text(qtdCaracteres);
+
+    });
+}
+
+function inicializaCronometro() {
+
+    var tempoRestante = $("#tempo-digitacao").text();
+
+    campo.one("focus", function(){
+        var cronometroID = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
+
+// Botao reiniciar
+// Com jQuery
+// $("#botao-reiniciar").on("click", function(){
+//     console.log("cliquei");
+// });
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-de-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+};
